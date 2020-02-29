52d7f8271eff001359570d3f28f84160e37b8213
Acronite <49061805+Acronite@users.noreply.github.com>
2020-02-29 23:04:28 +0100

    Add files via upload

--- scumlogs.py
+++ scumlogs.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
-# g-portal logs downloader for scum servers
-# by GAMEBotLand.com
+# Changelog 27-02-2020:
+# - Fixed the log retrieval error.
+# - The script does now continue each log in a single file instead of keep making chunks like G-portal stores them.
+# Updated by Zodiac script originally created by Sergiocr.
 
 import json
 import asyncio
 from bs4 import BeautifulSoup
 from aiocfscrape import CloudflareScraper
-from configparser import RawConfigParser
+from configparser import ConfigParser
 from datetime import datetime
 
 def log(text):
@@ -22,24 +23,28 @@ def help():
     print('  leave the rest of the parameters as is\n')
 
 def load_configini():
-    config = RawConfigParser()
-    with open('scumlogs.ini', 'r', encoding="utf-8") as f:
+    config = ConfigParser()
+    with open('scumlogs.ini', 'r') as f:
         config.read_file(f)
     global configini
     configini = dict(config['GPORTAL'])
 
 def save_configini():
-    parser = RawConfigParser()
+    parser = ConfigParser()
     parser.add_section('GPORTAL')
     for key in configini.keys():
         parser.set('GPORTAL', key, configini[key])
-    with open('scumlogs.ini', 'w', encoding="utf-8") as f:
+    with open('scumlogs.ini', 'w') as f:
         parser.write(f)
 
 
 async def read_logs():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+
+    }
     values = ('user','password','serverid','loc','folder','admin_file','admin_line','chat_file','chat_line','kill_file','kill_line','login_file','login_line','violations_file','violations_line')
-    print('scumlogs v1.0, scum server logs downloader from gportal\nby htttps://GAMEBotLand.com')
+    print('scumlogs v1.0, scum server logs downloader from gportal\nby https://GAMEBotLand.com')
     try:
         load_configini()
     except:
@@ -48,9 +53,6 @@ async def read_logs():
     for value in values:
         if value not in configini:
             configini[value] = ''
-    if configini['folder'] != '':
-        if configini['folder'][-1:] != '/' and configini['folder'][-1:] != '\\':
-            configini['folder'] = configini['folder'] + '/'
     save_configini()
 
     if configini['loc'] == 'com':
@@ -59,14 +61,13 @@ async def read_logs():
         loc = 'us'
     URL_LOGIN = 'https://id2.g-portal.com/login?redirect=https://www.g-portal.{}/en/gportalid/login?'.format(configini['loc'])
     URL_LOGS = 'https://www.g-portal.{}/en/scum/logs/{}'.format(configini['loc'], configini['serverid'])
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'}
-    
+
     async with CloudflareScraper() as session:
         try:
             log('connecting g-portal...')
             payload = {'_method': 'POST', 'login': configini['user'], 'password': configini['password'],
                        'rememberme': '1'}
-            async with session.post(URL_LOGIN, headers=headers, data=payload) as raw_response:
+            async with session.post(URL_LOGIN, data=payload, headers=headers) as raw_response:
                 response = await raw_response.text()
             async with session.get(URL_LOGS, headers=headers) as raw_response:
                 response = await raw_response.text()
@@ -74,6 +75,10 @@ async def read_logs():
             select = html.find('div', {'class': 'wrapper logs'})
             loglist = select['data-logs']
             logs = json.loads(loglist)
+
+            if configini['folder'] != '':
+                if configini['folder'][-1:] != '/' and configini['folder'][-1:] != '\\':
+                    configini['folder'] = configini['folder'] + '/'
 
             for i in range(len(logs)):
                 getid = logs["file_" + str(i + 1)]
@@ -84,11 +89,12 @@ async def read_logs():
                     if id < configini[type + '_file']:
                         continue
                 payload = {'_method': 'POST', 'load': 'true', 'ExtConfig[config]': getid}
-                async with session.post(URL_LOGS, headers=headers, data=payload) as raw_response:
+                async with session.post(URL_LOGS, data=payload, headers=headers) as raw_response:
                     response = await raw_response.text()
                 content = json.loads(response)
                 lines = content["ExtConfig"]["content"].splitlines()
-                filename = configini['folder'] + id
+                #filename = configini['folder'] + id
+                filename = configini['folder'] + type
                 file = open(filename, "a+", encoding='utf-8')
                 found = False
                 writing = False
